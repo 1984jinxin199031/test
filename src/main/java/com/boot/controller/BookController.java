88a14c430e5635284bd8ba4878239ff6e4b3d4d5
1984jinxin199031 <1984jinxin199031>
2023-03-03 15:05:17 +0800

    test

--- src/main/java/com/boot/controller/BookController.java
+++ src/main/java/com/boot/controller/BookController.java
@@ -115,8 +115,6 @@ public class BookController {
                                 e.printStackTrace();
                             }
                         }
-                        //照片上传成功后返回照片信息到页面，用于更新页面显示的照片信息，便于删除是获取相关信息
-
                         String fileAddr = relativePath + fileName;
                         String fileAddrJson = "{'fileAddr':'"+ fileAddr + "'}";
                         return new Result(Code.FILE_OK,fileAddrJson,"文件保存成功");
